8adf43d2654bd0849ea8dd58df50cf1f608b69a0
Alexander <alexander.pinto@ucsp.edu.pe>
2021-07-15 22:50:48 -0500

    DB Settings

--- evaluation/settings.py
+++ evaluation/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'evaluation.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bsale_test',
+        'USER': 'bsale_test',
+        'PASSWORD': 'bsale_test',
+        'HOST': 'mdb-test.c6vunyturrl6.us-west-1.rds.amazonaws.com',
     }
 }
 
