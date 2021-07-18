4862fa442e214a1e3a25f473f45e00fd41e5dcef
Alexander <alexander.pinto@ucsp.edu.pe>
2021-07-17 19:24:35 -0500

    Frontend Branch

--- store/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from .models import Category
-from .models import Product
-
-admin.site.register(Category)
-admin.site.register(Product)
