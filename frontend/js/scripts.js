1bd3a4c443963854db524fdf2a61e99372377766
Alexander <alexander.pinto@ucsp.edu.pe>
2021-07-17 21:55:16 -0500

    Basic error handling and Frontend refactoring

--- frontend/js/scripts.js
+++ frontend/js/scripts.js
@@ -1,8 +1,7 @@
-var pagination = {
+// Global pagination object values
+var PAGINATION = {
     keyword: '',
     totalRecords : 0,
-    //records : [],
-    //displayRecords : [],
     recPerPage : 12,
     page : 1,
     totalPages : 0,
@@ -12,38 +11,60 @@ var pagination = {
 
  const URL_QUERY = "http://localhost:8000/list/";
 
+jQuery.ajaxSetup({
+    beforeSend: function() {
+       $('#loader').show();
+    },
+    complete: function(){
+       $('#loader').hide();
+    },
+    success: function() {}
+});
+
 $(document).ready(function() {
-    url = URL_QUERY + '?page=1&search=';
-    $.ajax({
-        url: URL_QUERY
-    }).then( data => {
-            getValuesPagination(data,'');
-            renderProduct (data);
-            renderPagination();
-        });
 
-    // $( () => searchProducts(''));
+    //Initial Query
+    $( () => searchProducts(''));
 
+    //Show Search Modal
     $('#searchToggleButton').click( () => {
         $('#searchModal').modal('show');
     });
 
+    //Search Button
     $('#searchButton').click( () => {
-        //alert("Búsqueda: " + $('#searchKeyword').val());
         searchProducts('?page=1&search=' + $('#searchKeyword').val());
         $('#searchModal').modal('hide');
         return false;
     });
 });
 
-function renderProduct(data) {
-    // console.log(data);
-    $("#products").empty();
+function searchProducts(keyword = '', page = 1) {
+    // Search Products related to keyword from REST API, updates pagination,
+    // render Products card and pagination bar
 
+    const url = URL_QUERY + keyword;
+    // console.log(url);
+    $("#products").append('<h4 class="text-primary" id="loader">Loading...</h4>');
+    $.ajax({
+        url: url
+    }).then( data => {
+        getValuesPagination(data, keyword, page);
+        renderProduct (data);
+        renderPagination();
+    }).fail( () => {
+        renderError();
+    });
+}
+
+function renderProduct(data) {
+    // Render products Cards from data object
+
+    $("#products").empty();
     {
         data.results.map( (product) => {
             $("#products").append(
-                `<div class="card" style="width: 16rem;">
+                `<div class="card m-2" style="width: 16rem;">
                         <img class="card-img-top" src="` + product.url_image+ `" alt="`+ product.name + `">
                         <div class="card-body">
                             <p class="card-text text-center">` + product.name + `</p>
@@ -61,42 +82,36 @@ function renderProduct(data) {
     } 
 }
 
-function searchProducts(keyword = '', page = 1) {
-    const url = URL_QUERY + keyword;
-    console.log(url);
-    $.ajax({
-        url: url
-    }).then( data => {
-        getValuesPagination(data, keyword, page);
-        renderProduct (data);
-        renderPagination();
-    });
-}
-
 function getValuesPagination(data, keyword, page = 1) {
-    pagination.keyword = keyword;
-    pagination.totalRecords = data.count;
-    pagination.next = data.next;
-    pagination.previous = data.previous;
-    pagination.totalPages = Math.ceil(pagination.totalRecords/pagination.recPerPage);
-    pagination.page = page;
+    // Update Pagination Object values
+
+    PAGINATION.keyword = keyword;
+    PAGINATION.totalRecords = data.count;
+    PAGINATION.next = data.next;
+    PAGINATION.previous = data.previous;
+    PAGINATION.totalPages = Math.ceil(PAGINATION.totalRecords/PAGINATION.recPerPage);
+    PAGINATION.page = page;
 }
 
 function renderPagination() {
+    // Render pagination navbar from pagination object values
+
     var $pag = $('#paginationBar');
-    let prev = pagination.page - 1;
-    let next = pagination.page + 1;
+    let prev = PAGINATION.page - 1;
+    let next = PAGINATION.page + 1;
     $pag.empty();
     
+    // Previous pagination button
     $pag.append(
         `<li class="page-item">
-            <a onClick="searchProducts('?page=` + prev + '&search=' + pagination.keyword + `',` + prev + `)"
+            <a onClick="searchProducts('?page=` + prev + '&search=' + PAGINATION.keyword + `',` + prev + `)"
                 class="page-link" role="button">Previous</a>
         </li>`
         );
     
-    for (let i = 1; i <= pagination.totalPages; i++){
-        if (pagination.page === i){
+    // Page numbers pagination buttons
+    for (let i = 1; i <= PAGINATION.totalPages; i++){
+        if (PAGINATION.page === i){
             $pag.append(
             `
             <li class="page-item active">
@@ -107,21 +122,30 @@ function renderPagination() {
         else {
             $pag.append(
                 `<li class="page-item">
-                <a onClick="searchProducts('?page=` + i + '&search=' + pagination.keyword + `',` + i + `)"
+                <a onClick="searchProducts('?page=` + i + '&search=' + PAGINATION.keyword + `',` + i + `)"
                     class="page-link" role="button">` + i + `</a>
                 </li>`
             );
         }
     }
+
+    // Next pagination button
     $pag.append(
         `<li class="page-item">
-            <a onClick="searchProducts('?page=` + next + '&search=' + pagination.keyword + `',` + next + `)"
+            <a onClick="searchProducts('?page=` + next + '&search=' + PAGINATION.keyword + `',` + next + `)"
                 class="page-link" role="button">Next</a>
         </li>`
     );
-
-    if (pagination.previous === null)
+    
+    // Disable next 'n previous buttons according to actual page
+    if (PAGINATION.previous === null)
         $("#paginationBar li:first").addClass("disabled");
-    if (pagination.next === null)
+    if (PAGINATION.next === null)
         $("#paginationBar li:last").addClass("disabled");        
 }
+
+function renderError(){
+    var $pag = $('#paginationBar');
+    $pag.append( '<h3 class="text-danger">Failed to load products...</h3>')
+}
+
