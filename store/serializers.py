5ea85ac9669f34e28ac65a8cfefed9e416c19cfd
Alexander <alexander.pinto@ucsp.edu.pe>
2021-07-24 14:31:06 -0500

    Ordering Filter + Better comments

--- store/serializers.py
+++ store/serializers.py
@@ -5,6 +5,13 @@ from rest_framework import serializers
 from .models import Product, Category
 
 class CategorySerializer(serializers.ModelSerializer):
+    '''
+    Output example:
+    {
+        id : 01,
+        name : bebida
+    }
+    '''
     class Meta:
         model = Category
         fields = [
@@ -13,8 +20,21 @@ class CategorySerializer(serializers.ModelSerializer):
         
 
 class ProductSerializer(serializers.ModelSerializer):
+    '''
+    Output example:
+    {
+            "id": 68,
+            "name": "Bebida Sprite 1 Lt",
+            "url_image": null,
+            "price": 1250.0,
+            "discount": 10,
+            "category": {
+                "id": 4,
+                "name": "bebida"
+            }
+        },
+    '''
     category = CategorySerializer(many = False)
-    # category = serializers.StringRelatedField(many = False) 
 
     class Meta:
         model = Product
@@ -28,7 +48,20 @@ class ProductSerializer(serializers.ModelSerializer):
         ]
 
 class GroupSerializer(serializers.ModelSerializer):
-
+    '''
+    Output example:
+    {
+            "bebida energetica": [
+                {
+                    "id": 5,
+                    "name": "ENERGETICA MR BIG",
+                    "url_image": "https://dojiw2m9tvv09.cloudfront.net/11132/product/misterbig3308256.jpg",
+                    "price": 1490.0,
+                    "discount": 20,
+                    "category_id": 1
+                },
+                ...
+    '''
     def to_representation(self, data):
         rep = { data.name : [prod for prod in Product.objects.filter(category_id=data.id).values()] }
         return rep
--- store/views.py
+++ store/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets, pagination, generics, filters
 
-# Create your views here.
-
 from .serializers import CategorySerializer, ProductSerializer, GroupSerializer
 from .models import Product, Category
 
@@ -13,25 +11,42 @@ class ProductPagination(pagination.PageNumberPagination):
     page_size = 12
 
 class ProductViewSet(viewsets.ModelViewSet):
+    '''
+    List of products: 
+    - 12 products per page
+    '''
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     pagination_class = ProductPagination
 
 class ProductList(generics.ListAPIView):
+    '''
+    List of products: 
+    - 12 products per page
+    - Regex search in product
+    - Filter by category
+    - Order by name, price, category_id
+    '''
     model = Product
     pagination_class = ProductPagination
     serializer_class = ProductSerializer
-    filter_backends = [filters.SearchFilter]
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
     search_fields = ['$name']
+    ordering_fields = ['name', 'price', 'category_id']
 
     def get_queryset(self):
         try:
             cat_id = int(self.kwargs['category_id'])
             return Product.objects.filter(category_id = cat_id)
         except:
-            return Product.objects.all().order_by('category_id')
+            return Product.objects.all()
 
 class GroupViewSet(viewsets.ModelViewSet):
+    '''
+    List of products grouped by category: 
+    - 1 category per page
+    - Regex search in category
+    '''
     queryset = Category.objects.all()
     serializer_class = GroupSerializer
     pagination_class = GroupPagination
@@ -39,5 +54,8 @@ class GroupViewSet(viewsets.ModelViewSet):
     search_fields = ['$name']
 
 class CategoryViewSet(viewsets.ModelViewSet):
+    '''
+    List of all categories of products
+    '''
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
