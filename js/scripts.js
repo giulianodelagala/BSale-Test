18d661986780c26579041ba8adba4915242914cd
Alexander <alexander.pinto@ucsp.edu.pe>
2021-07-24 14:38:26 -0500

    UI impro, ordering options, responsive images

--- js/scripts.js
+++ js/scripts.js
@@ -7,12 +7,15 @@ var PAGINATION = {
     totalPages : 0,
     next : null,
     previous: null,
-    category: 0
+    category: 0,
 }
 
-var CATEGORIES = {}
+// Search results ordering
+var ORDERING = 'category_id';
+// Product Categories
+var CATEGORIES = { 0 : 'Todo Producto'}
 
-const baseURL = "https://bs-backend.herokuapp.com/";
+const baseURL = "http://localhost:8000/";
 const URL_QUERY = baseURL + "list/";
 
 jQuery.ajaxSetup({
@@ -30,8 +33,6 @@ $(document).ready(function() {
     //Initial Query
     $( () => searchProducts(category=0, page = 1, search=''));
     $( () => getCategories());
-
-    // $( () =>renderCategoriesMenu());
 
     //Show Search Modal
     $('#searchToggleButton').click( () => {
@@ -57,8 +58,10 @@ function searchProducts(category = 0, page = 1, search = '') {
     let cat_path = (category === 0) ? '' : category + '/';
     let search_path = 'search=' + search;
     let page_path = '?page=' + page;
+    let order_path = 'ordering=' + ORDERING;
 
-    const url = URL_QUERY + cat_path + page_path + '&' + search_path;
+    const url = URL_QUERY + cat_path + page_path +
+        '&' + search_path + '&' + order_path;
     console.log(url);
     $("#products").append(
         `<div class="container justify-content-center col-12" id="loader">
@@ -95,7 +98,7 @@ function getCategories() {
 
 function renderProduct(data) {
     // Render products Cards from data object
-    // console.log(JSON.stringify(data.results))
+
     $("#products").empty();
     {   var last_category = '';
         data.results.map( (product => {
@@ -106,6 +109,12 @@ function renderProduct(data) {
                     </div>`)
                 last_category = product.category.name;
             }
+            // Controling not available images
+            if (product.url_image == null ||
+                product.url_image == ''){
+                product.url_image = 'images/image-not-available.png';
+            }
+            // Insert Card product
             $("#products").append(
                 `<div class="card m-2" style="width: 16rem;">
                         <img class="card-img-top" src="` + product.url_image+ `" alt="`+ product.name + `">
@@ -141,7 +150,7 @@ function setValuesPagination(data, category, search, page) {
 
 function renderPagination() {
     // Render pagination navbar from pagination object values
-    var $pag = $('#paginationBar');
+    var $pag = $('.paginationBar');
     let prev = PAGINATION.page - 1;
     let next = PAGINATION.page + 1;
     // console.log(JSON.stringify(PAGINATION))
@@ -204,7 +213,7 @@ function renderCategoriesMenu() {
     $('#categories_menu').append(
         `<li><a id="cat_` + 0 + `"
             onClick="setActiveAndSearch(` + 0 + `)"
-            class="dropdown-item page-link" role="button">` + 'Todo producto' + `</a>
+            class="dropdown-item" role="button">` + 'Todo producto' + `</a>
         </li>`
     );
     len = Object.keys(CATEGORIES).length;
@@ -212,10 +221,34 @@ function renderCategoriesMenu() {
         $('#categories_menu').append(
             `<li><a id="cat_` + i+ `"
                 onClick="setActiveAndSearch(` + i + `)"
-                class="dropdown-item page-link" role="button">` + CATEGORIES[i] + `</a>
+                class="dropdown-item" role="button">` + CATEGORIES[i] + `</a>
             </li>`
         );    
     }
+}
+
+function setOrdering(ordering='category_id') {
+    // Set active ordering option and in dropdown menu
+
+    let ordering_title = {
+        'category_id' : 'Category',
+        'name' : 'A - Z',
+        '-name' : 'Z - A',
+        'price' : 'Lower price first',
+        '-price' : 'Higher price first',
+    }
+
+    $('#ordering_menu').children().children().removeClass("active");
+    $('#ord-' + ordering).addClass("active");
+
+    $('#selectedOrdering').text(ordering_title[ordering]);
+
+    ORDERING = ordering;
+
+    searchProducts(
+        category= PAGINATION.category,
+        page= 1 ,
+        search= PAGINATION.search );
 }
 
 function setActiveAndSearch(category=0) {
@@ -224,6 +257,9 @@ function setActiveAndSearch(category=0) {
 
     $('#categories_menu').children().children().removeClass("active");
     $('#cat_' + category).addClass("active");
+
+    $('#selectedCategory').text(' ' + CATEGORIES[category.toString()]);
+
     searchProducts(
         category= category,
         page= 1 ,
